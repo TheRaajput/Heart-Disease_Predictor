e7875a0492e9e3a3c3bb67566ee42b1d4791a062
Yasharth Singh Parihar <54817396+TheRaajput@users.noreply.github.com>
2020-10-14 19:07:25 +0530

    Add files via upload

--- datasetDB.py
+++ datasetDB.py
@@ -3,13 +3,13 @@ import sqlite3
 con = sqlite3.connect("dataset.db")
 print('Database Opened')
 
-con.execute("create table Heartdisease(SrNo INTEGER PRIMARY KEY AUTOINCREMENT,age INTEGER,gender INTEGER,chestPain INTEGER,bloodPressure INTEGER,cholestrol INTEGER,bsugar INTEGER,ecg INTEGER,heartRate INTEGER,cPain INTEGER,STdepression INTEGER,slopeST INTEGER,majorVessel INTEGER,ThalScore INTEGER, result INTEGER)")
+# con.execute("create table Heartdisease(SrNo INTEGER PRIMARY KEY AUTOINCREMENT,age INTEGER,gender INTEGER,chestPain INTEGER,bloodPressure INTEGER,cholestrol INTEGER,bsugar INTEGER,ecg INTEGER,heartRate INTEGER,cPain INTEGER,STdepression INTEGER,slopeST INTEGER,majorVessel INTEGER,ThalScore INTEGER, result INTEGER)")
             
-print("Table created")
-# cur = con.cursor()
+# print("Table created")
+cur = con.cursor()
 
-# cur.execute("SELECT * from Heartdisease")
+cur.execute("SELECT * from Heartdisease")
 
-# result = cur.fetchall()
-# print(result)
+result = cur.fetchall()
+print(result)
 con.close()
